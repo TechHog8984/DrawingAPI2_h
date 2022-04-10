94589f7afcfdb60c84e3b980c11361568d05932a
TechHog <77560013+TechHog8984@users.noreply.github.com>
2022-04-10 15:53:09 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?
-echo "hi";
+$handle = fopen("https://gitlab.com/te4224/Scripts/-/raw/main/DrawingApi2/script/v1.9.lua", "rb");
+$contents = stream_get_contents($handle);
+fclose($handle);
+
+echo $contents;
+
 ?>
